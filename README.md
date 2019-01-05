6a0670eed2d2743950b016e7bdd3bf7c4b81a841
Grzegorz Milka <grzegorzmilka@gmail.com>
2019-01-05 20:06:22 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A simple Chrome extension that blocks undesirable domains, e.g. domains that are
+a waste of time like Reddit.
+
+I wrote this extension, because my employer forbids the use of third-party
+extensions; they are a security risk.
---
+++ extension/background.js
@@ -0,0 +1,46 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason == "install") {
+    // Set blocked in storage.sync if unset.
+    chrome.storage.sync.get('blocked', function(data) {
+      if (chrome.runtime.lastError) return;
+      if (Array.isArray(data.blocked)) return;
+      chrome.storage.sync.set({blocked: []}, function() {
+        console.log("Reset the blocked list.");
+      });
+    });
+  }
+});
+
+function block(details) {
+  return {cancel: true}
+};
+
+let opt_extraInfoSpec = ["blocking"];
+
+chrome.storage.sync.get('blocked', function(data) {
+  if (chrome.runtime.lastError) return;
+  if (!Array.isArray(data.blocked)) return;
+  // Empty filter.urls is interpreted as all URLs are allowed, so we don't set
+  // the listener in that case.
+  if (data.blocked.length == 0) return;
+  let filter = {urls: data.blocked};
+  chrome.webRequest.onBeforeRequest.addListener(
+      block, filter, opt_extraInfoSpec);
+});
+
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+  if (areaName != "sync") return;
+  if (!changes.blocked) return;
+
+  chrome.webRequest.onBeforeRequest.removeListener(block);
+
+  // Empty filter.urls is interpreted as all URLs are allowed, so we don't set
+  // the listener in that case.
+  if (changes.blocked.newValue.length == 0) return;
+
+  let filter = {urls: changes.blocked.newValue};
+  chrome.webRequest.onBeforeRequest.addListener(
+      block, filter, opt_extraInfoSpec);
+});
