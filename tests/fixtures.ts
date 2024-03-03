198107223388910bb552ee481e3fbe671129d37e
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-03-03 13:16:50 +0100

    chore: clean up fixtures.ts

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -18,14 +18,6 @@ export const test = base.extend<{
     await context.close();
   },
   extensionId: async ({ context }, use) => {
-    /*
-    // for manifest v2:
-    let [background] = context.backgroundPages()
-    if (!background)
-      background = await context.waitForEvent('backgroundpage')
-    */
-
-    // for manifest v3:
     let [background] = context.serviceWorkers();
     if (!background) background = await context.waitForEvent("serviceworker");
 
