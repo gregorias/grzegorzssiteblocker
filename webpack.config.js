f73e336e08dff0c57f8203b7080a345c454f60d1
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-03-03 11:55:09 +0100

    chore: install React

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
         test: /\.m?tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
