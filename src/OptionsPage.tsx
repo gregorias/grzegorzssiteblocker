b4a29238b5c001bb8392b87246e0ca446c11e27d
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-03-08 19:04:46 +0100

    feat: apply Theme

--- src/OptionsPage.tsx
+++ src/OptionsPage.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+import { theme } from "./theme";
 import { Rule } from "./rule";
 import { RuleList } from "./RuleList";
 import { storage } from "./storage";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
 
 interface OptionsPageProps {}
 
@@ -41,13 +43,15 @@ export function OptionsPage(_props: OptionsPageProps): React.ReactElement {
 
   return (
     <>
-      <RuleList
-        rules={rules}
-        onRulesChange={storage.setRules}
-        onAddRule={() => {
-          storage.setRules([...rules, new Rule(false, "")]);
-        }}
-      />
+      <ThemeProvider theme={theme}>
+        <RuleList
+          rules={rules}
+          onRulesChange={storage.setRules}
+          onAddRule={() => {
+            storage.setRules([...rules, new Rule(false, "")]);
+          }}
+        />
+      </ThemeProvider>
     </>
   );
 }
