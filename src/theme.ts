8ecf78b9d1a2b2cad92c184efe72bdacf14616fe
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-03-08 18:39:46 +0100

    feat: add MUI theme

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// The material UI theme for the extension.
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f44336",
+    },
+  },
+});
